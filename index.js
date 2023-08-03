2238fd88f7a3ccc469c99b90f394d4c4b5ef022a
mine9607 <81383389+mine9607@users.noreply.github.com>
2023-08-02 23:50:21 -0500

    added feature to post composed blogs and render to homepage

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import ejs from "ejs";
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -11,12 +12,35 @@ const contactContent =
 
 const app = express();
 const port = 3000;
+const posts = [];
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  const title = "Home";
+  res.render("index.ejs", { title: title, content: homeStartingContent, posts: posts });
+});
+
+app.get("/about", (req, res) => {
+  const title = "About";
+  res.render("index.ejs", { title: title, content: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  const title = "Contact";
+  res.render("index.ejs", { title: title, content: contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  const title = "Compose";
+  res.render("compose.ejs", { title: title });
+});
+
+app.post("/compose", (req, res) => {
+  const post = { title: req.body.blogTitle, text: req.body.blogText };
+  posts.push(post);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
